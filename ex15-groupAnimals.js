bb918cebd2bb5c729ec86c01773af731665382c3
abdmhamzah <iamhamzaham@gmail.com>
2020-02-07 11:14:10 +0700

    update tugas 15

--- ex15-groupAnimals.js
+++ ex15-groupAnimals.js
@@ -4,20 +4,33 @@ function groupAnimals(animals) {
 
     // sort animal sesuai abjad
     // animals.sort()
+    for (let k = 0; k < animals.length; k++) {
+        console.log(animals[k][0]);
+        for (let l = 0; l < animals.length; l++) {
+            if (animals[l] > animals[l+1]){
+                var temp = animals[l]
+                animals[l] = animals[l+1]
+                animals[l+1] = temp
+            }
+        }
+    }
+    console.log(animals);
+    
     
     var hasil = []
     for (var i = 0; i < animals.length; i++) {
         var cek = false
-        var index
         for (var j = 0; j < hasil.length; j++) {
-            if(hasil[j][0] === animals[i][0]){
+            if(hasil[j][0][0] === animals[i][0]){
                 hasil[j].push(animals[i])
                 animals[i] = ''
             }
         }
-        if (cek === false){
+        
+        if (cek === false && animals[i] !== ''){
             hasil.push([animals[i]])
         }
+        
     }
     
 
